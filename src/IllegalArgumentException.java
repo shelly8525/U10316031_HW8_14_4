4008c2dca51269e17da4456a87c4594041a15d85
shelly8525 <shelly8525@yahoo.com.tw>
2015-05-17 20:14:23 +0800

    寫完get set 方法monthlyPayment()和 totalPayment()方法回傳值

--- src/IllegalArgumentException.java
+++ src/IllegalArgumentException.java
@@ -1,4 +1,5 @@
-public class IllegalArgumentException {
+
+public class IllegalArgumentException extends Exception{
 	public static void main(String[] args){
 		try {
 			new NewLoan(7.5,30,20000);
@@ -27,10 +28,49 @@ class NewLoan{
 			throw new IllegalArgumentException("Annual interest rate must be positive.");
 		if(numOfYears <= 0)
 			throw new IllegalArgumentException("Number of years must be positive.");
-		if(loanAmout <= 0)
+		if(loanAmount <= 0)
 			throw new IllegalArgumentException("Loan amount must be positive.");
 		setAnnualInterestRate(annualInterestRate);
 		setNumOfYears(numOfYears);
 		setLoanAmount(loanAmount);
+	}
+	
+	public double getAnnualInterestRate(){
+		return annualInterestRate;
+	}
+	
+	public void setAnnualInterestRate(double annualInterestRate){
+		if(annualInterestRate <= 0)
+			throw new IllegalArgumentException("Annual interest rate must be positive.");
+		this.annualInterestRate = annualInterestRate;
+	}
+	
+	public int getNumOfYears(){
+		return numOfYears;
+	}
+	
+	public void setNumOfYears(int numOfYears){
+		if(numOfYears <= 0)
+			throw new IllegalArgumentException("Number of years must be positive.");
+		this.numOfYears = numOfYears;
+	}
+	
+	public double getLoanAmount() {
+	    return loanAmount;
+	}
+	
+	public void setLoanAmount(double loanAmount) {
+		if (loanAmount <= 0)
+		     throw new IllegalArgumentException("Loan amount must be positive.");
+		this.loanAmount = loanAmount;
+	}
+	
+	public double monthlyPayment(){
+		double monthlyInterestRate = annualInterestRate / 1200;
+		return loanAmount * monthlyInterestRate / (1 - (Math.pow(1/(1 + monthlyInterestRate),numOfYears * 12)));
+	}
+	
+	public double totalPayment() {
+	    return monthlyPayment() * numOfYears * 12;
 	}
 }
